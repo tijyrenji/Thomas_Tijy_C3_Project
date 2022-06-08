387a877eb4b3e8024becc9ae9a2d468222a06093
Tijy Renji <tijitk4i@gmail.com>
2022-06-08 13:36:10 +0530

    Initial stub

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Do you like to Search Restaurant ? << Type 1 for Yes, 2 for No " );
+
+    }
+}
